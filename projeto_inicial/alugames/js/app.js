0abbf790e79b452a6eacc544872c7fa3237e9921
Iara Ribeiro <iararp05@gmail.com>
2024-06-13 12:10:20 -0300

    Contando jogos alugados

--- projeto_inicial/alugames/js/app.js
+++ projeto_inicial/alugames/js/app.js
@@ -1,20 +1,30 @@
+let alugados = 0;
+jogosAlugados();
+
 function alterarStatus(id){
     let elemento = document.getElementById('game-'+id);
     let imagem = elemento.querySelector('.dashboard__item__img');
     let botao = elemento.querySelector('.dashboard__item__button');
+    let nome = elemento.querySelector('.dashboard__item__name');
 
     if(botao.classList.contains('dashboard__item__button--return')){
-        if(confirm("Tem certeza que deseja devolver o jogo?")){
+        if(confirm("Tem certeza que deseja devolver o jogo "+nome.textContent+'?')){
             botao.classList.remove('dashboard__item__button--return');
             imagem.classList.remove('dashboard__item__img--rented');
             botao.textContent = 'Alugar';
-        }else{
-            return;
+            alugados--;
+            jogosAlugados();
         }
-        
     }else{
         botao.classList.add('dashboard__item__button--return');
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver'
+        alugados++;
+        jogosAlugados();
     }
+}
+
+function jogosAlugados(){
+    alugados = document.querySelectorAll('.dashboard__item__button--return').length;
+    document.getElementById('alugados').innerHTML = 'Total de jogos alugados: ' +alugados;
 }
